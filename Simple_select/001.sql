849dd51ea6e45ad0c7ac0ac3c1548717dea81bac
Ivan Boyko <112623912+Ivanbyone@users.noreply.github.com>
2023-12-04 17:49:37 +0300

    Add files via upload

---
+++ Simple_select/001.sql
@@ -0,0 +1,21 @@
+SELECT *
+FROM customers;
+
+SELECT contact_name, city
+FROM customers;
+
+SELECT order_id, 
+shipped_date - order_date AS difference
+FROM orders;
+
+SELECT DISTINCT city
+FROM customers;
+
+SELECT DISTINCT city, country
+FROM customers;
+
+SELECT COUNT(*)
+FROM customers;
+
+SELECT COUNT(DISTINCT country)
+FROM customers
